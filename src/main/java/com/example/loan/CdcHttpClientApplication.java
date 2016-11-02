0a2734b9fb02433595f0e39fd1cc3180c7b75d50
Bruce Baron <baron.br@gmail.com>
2016-11-02 14:28:11 -0400

    v0

---
+++ src/main/java/com/example/loan/CdcHttpClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcHttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcHttpClientApplication.class, args);
+	}
+}
